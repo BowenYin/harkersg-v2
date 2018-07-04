3302071a79c6d8649fd44f8c0d6ac42dae08812d
Bowen Yin <bowenvictoryin@gmail.com>
2018-07-04 10:24:55 -0700

    finished courses script

--- public/scripts/courses.js
+++ public/scripts/courses.js
@@ -1,13 +1,49 @@
-app.controller("CoursesControl",function($scope,$rootScope) {
+app.controller("CoursesControl", function($scope, $rootScope, $mdMedia, $mdToast) {
 	$rootScope.navItem="courses";
 	$rootScope.pageTitle="Course Catalog";
-	$rootScope.tabName="Courses - HarkerSG";
-	$scope.loaded=false;
+	$rootScope.tabName="Course Catalog - HarkerSG";
+	$scope.loading=true;
+	$scope.unsaved=false;
+	$scope.search="";
+	$scope.gtXs=$mdMedia("gt-xs");
 	db.ref("/courses").once("value").then(function(snapshot) {
-		$scope.courses=snapshot.val();
-		$scope.loaded=true;
+		$scope.list=snapshot.val();
 		$scope.$apply();
-	}).catch(function(error) {
-		$scope.loaded=true;
+	}).finally(function() {
+		$scope.loading=false;
 	});
+	$scope.update=function(course) {
+		if ($scope.selected[course]!==true) delete $scope.selected[course];
+		$scope.unsaved=!angular.equals($scope.saved, $scope.selected);
+		$scope.max=Object.keys($scope.selected).length>7;
+	};
+	$scope.reset=function() {
+		$scope.selected={};
+		Object.assign($scope.selected, $scope.saved);
+		$scope.update();
+	};
+	$scope.save=function() {
+		$scope.loading=true;
+		let arr=[];
+		for (let name in $scope.selected)
+			if ($scope.selected[name]===true) arr.push(name);
+		var updateCourses=functions.httpsCallable("updateCourses");
+		$mdToast.show($mdToast.simple().textContent("Saving courses. This may take up to 30 seconds."));
+		updateCourses({courses: arr}).then(function(result) {
+			$scope.loading=false;
+			$mdToast.hide();
+			window.location.reload(false);
+		}).catch(function(error) {
+			$scope.loading=false;
+			$mdToast.hide();
+			console.log(error);
+			$mdToast.show(
+				$mdToast.simple()
+				.textContent("An unexpected error has occurred.")
+				.hideDelay(4000)
+				.action("OK")
+				.highlightAction(true)
+			);
+		});
+	};
 });
