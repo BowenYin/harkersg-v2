3d2dc47fc9032a0fb6760967e33fce2360e98508
Bowen Yin <bowenvictoryin@gmail.com>
2018-07-04 10:23:41 -0700

    added function for saving courses

--- functions/index.js
+++ functions/index.js
@@ -35,8 +35,8 @@ exports.restore=functions.https.onRequest((req, res)=>{
   var data=change.after.data();
   return data.user.get().then(function(doc) { data.user.update({lastFb: new Date()}); });
 });*/
-exports.newUser=functions.firestore.document("users/{userId}").onCreate(event=>{
-  return change.after.ref.update({
+exports.newUser=functions.firestore.document("users/{userId}").onCreate((snapshot, context)=>{
+  return snapshot.ref.update({
     flags: 0,
     //lastAdd: new Date("January 1, 2017 00:00:00"),
     lastFb: new Date("January 1, 2017 00:00:00"),
@@ -48,6 +48,21 @@ exports.manageUser=functions.https.onCall((data, context)=>{
     firestore.collection("users").doc(context.auth.uid).set({name: context.auth.token.name, email: context.auth.token.email});
   }
 });
+exports.updateCourses=functions.https.onCall((data, context)=>{
+  if (!context.auth)
+    throw new functions.https.HttpsError("unauthenticated", "Not authenticated.");
+  if (!context.auth.token.email.match(/.+(?:@students\.|@staff\.|@)harker.org$/))
+    throw new functions.https.HttpsError("permission-denied", "Must use a school email address.");
+  if (data.courses.constructor!==Array || data.courses.length>7)
+    throw new functions.https.HttpsError("invalid-argument", "Please check data again.");
+  return firestore.collection("users").doc(context.auth.uid).update({
+    courses: data.courses,
+  }).then(()=>{
+    return data.courses;
+  }).catch(error=>{
+    throw new functions.https.HttpsError("unknown", "An unexpected error occurred.", error);
+  });
+});
 exports.addSG=functions.https.onCall((data, context)=>{
   try {
     if (!context.auth)
@@ -58,28 +73,28 @@ exports.addSG=functions.https.onCall((data, context)=>{
       throw new functions.https.HttpsError("failed-precondition", "Please accept the honor code.");
     if (typeof data.title!="string" || data.title.length<5 || data.title.length>50 ||
         typeof data.info!="string" || data.info.length>100 ||
-        typeof data.url!="string" || data.url.indexOf("http")!=0 || data.url.length<18 || data.url.length>300 ||
+        typeof data.url!="string" || data.url.indexOf("http")!=0 || data.url.length<18 || data.url.length>250 ||
         typeof data.showName!="boolean")
       throw new functions.https.HttpsError("invalid-argument", "Please check data again.");
     const userDoc=firestore.collection("users").doc(context.auth.uid);
-    userDoc.get().then(doc=>{
+    return userDoc.get().then(doc=>{
       const user=doc.data();
       if (user.flags>3)
         throw new functions.https.HttpsError("permission-denied", "Must have 3 or fewer flags to add study guides.", "User has "+user.flags+" flags.");
       const courseDoc=firestore.collection("courses").doc(data.course);
-      courseDoc.get().then(doc=>{
+      return courseDoc.get().then(doc=>{
         if (!doc.exists)
           throw new functions.https.HttpsError("not-found", "Course not found.");
-        if (data.folder!=null && !doc.data().folders.some(value=>{return value.title===data.folder;}))
+        if (data.folder!=null && !doc.data().folders.some(value=>{return value.time===data.folder;}))
           throw new functions.https.HttpsError("not-found", "Folder not found.");
-        courseDoc.collection("sg").add({
+        return courseDoc.collection("sg").add({
           title: data.title,
           info: data.info,
           url: data.url,
           folder: data.folder,
           flags: [],
           marks: [],
-          name: data.showName?context.auth.token.name||user.name:null,
+          name: data.showName?context.auth.token.name||user.name:"Anonymous",
           user: userDoc,
           time: admin.firestore.FieldValue.serverTimestamp(),
         });
@@ -96,23 +111,26 @@ exports.addFolder=functions.https.onCall((data, context)=>{
       throw new functions.https.HttpsError("unauthenticated", "Not authenticated.");
     if (!context.auth.token.email.match(/.+(?:@students\.|@staff\.|@)harker.org$/))
       throw new functions.https.HttpsError("permission-denied", "Must use a school email address.");
-    firestore.collection("users").doc(context.auth.uid).get().then(doc=>{
+    if (typeof data.title!="string" || data.title.length<4 || data.title.length>30)
+      throw new functions.https.HttpsError("invalid-argument", "Please check data again.");
+    return firestore.collection("users").doc(context.auth.uid).get().then(doc=>{
       const user=doc.data();
       if (user.flags>0)
         throw new functions.https.HttpsError("permission-denied", "Must have zero flags to add folders.", "User has "+user.flags+" flags.");
       const courseRef=firestore.collection("courses").doc(data.course);
-      var transaction=firestore.runTransaction(transaction=>{
+      return firestore.runTransaction(transaction=>{
         return transaction.get(courseRef).then(doc=>{
           if (!doc.exists)
             throw new functions.https.HttpsError("not-found", "Course not found.");
           let folders=doc.data().folders;
           folders.push({
-            time: new Date(),
+            time: new Date().getTime(),
             title: data.title,
           });
           transaction.update(courseRef, {folders: folders});
-          console.log('Folder "'+data.title+'" added to course '+data.course+' by '+context.auth.token.name+' (UID '+context.auth.uid+').');
         });
+      }).then(()=>{
+        console.log('Folder "'+data.title+'" added to '+data.course+' by '+context.auth.token.name+' (UID '+context.auth.uid+').');
       });
     });
   } catch (error) {
