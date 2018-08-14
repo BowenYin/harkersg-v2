3dbe6a41f0ba9c00f036d45cc71af3069f2acda0
Bowen Yin <bowenvictoryin@gmail.com>
2018-08-14 16:45:50 -0700

    condensed toast alerts to one line

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,7 @@
-app.controller("HomeControl", function($scope, $rootScope) {
+app.controller("HomeControl", function($rootScope) {
   $rootScope.navItem="home";
   $rootScope.pageTitle="HarkerSG Home";
-  $rootScope.tabName="HarkerSG Home";
+  $rootScope.tabName="HarkerSG - Harker Study Guides";
 });
 app.controller("NFControl", function($rootScope) {
   $rootScope.navItem=null;
@@ -12,63 +12,30 @@ app.controller("SettingsControl", function($rootScope, $scope, $mdToast) {
   $rootScope.navItem=null;
   $rootScope.pageTitle="User Settings";
   $rootScope.tabName="Settings - HarkerSG";
-  var cookies=document.cookie.split("; ");
-  for (var i=0; i<cookies.length; i++) {
-    if (cookies[i].split("=")[0]=="dark")
-      $scope.dark=cookies[i].split("=")[1]=="true";
-    if (cookies[i].split("=")[0]=="primary")
-      $scope.settings.primary=cookies[i].split("=")[1];
-    if (cookies[i].split("=")[0]=="accent")
-      $scope.settings.accent=cookies[i].split("=")[1];
-  }
+  $scope.dark=dark, $scope.primary=primary, $scope.accent=accent;
   $scope.darkTheme=function() {
-    document.cookie="dark="+$scope.dark.toString()+"; expires=Fri, 31 Dec 9999 12:00:00 UTC";
-    $mdToast.show(
-      $mdToast.simple()
-        .textContent("Refresh this page to see changes.")
-        .hideDelay(5000)
-        .action("Reload")
-        .highlightAction(true)
-        .position("top right")
-    ).then(function(response) {
-      if (response=="ok")
-        window.location.reload(false);
-    });
+    document.cookie="dark="+$scope.dark+"; expires=Fri, 31 Dec 9999 12:00:00 UTC";
+    $scope.showToast();
   };
   $scope.setPrimary=function() {
-    document.cookie="primary="+$scope.settings.primary.toString()+"; expires=Fri, 31 Dec 9999 12:00:00 UTC";
-    fs.collection("users").doc($scope.uid).update({ "settings.primary": $scope.settings.primary });
-    $mdToast.show(
-      $mdToast.simple()
-        .textContent("Refresh this page to see changes.")
-        .hideDelay(5000)
-        .action("Reload")
-        .highlightAction(true)
-        .position("top right")
-    ).then(function(response) {
-      if (response=="ok")
-        window.location.reload(false);
-    });
+    document.cookie="primary="+$scope.primary+"; expires=Fri, 31 Dec 9999 12:00:00 UTC";
+    $scope.showToast();
   };
   $scope.setAccent=function() {
-    document.cookie="accent="+$scope.settings.accent.toString()+"; expires=Fri, 31 Dec 9999 12:00:00 UTC";
-    fs.collection("users").doc($scope.uid).update({ "settings.accent": $scope.settings.accent });
+    document.cookie="accent="+$scope.accent+"; expires=Fri, 31 Dec 9999 12:00:00 UTC";
+    $scope.showToast();
+  };
+  $scope.showToast=function() {
     $mdToast.show(
-      $mdToast.simple()
-        .textContent("Refresh this page to see changes.")
-        .hideDelay(5000)
-        .action("Reload")
-        .highlightAction(true)
-        .position("top right")
+      $mdToast.simple().textContent("Refresh this page to see changes.").hideDelay(5000).action("Reload").highlightAction(true).position("top right")
     ).then(function(response) {
-      if (response==="ok")
-        window.location.reload(false);
+      if (response==="ok") window.location.reload(false);
     });
   };
 });
 app.controller("AboutControl", function($rootScope) {
   $rootScope.navItem="about";
-  $rootScope.pageTitle="About / Changelog";
+  $rootScope.pageTitle="About HarkerSG";
   $rootScope.tabName="About - HarkerSG";
 });
 app.controller("PrivacyControl", function($rootScope) {
@@ -78,47 +45,33 @@ app.controller("PrivacyControl", function($rootScope) {
 });
 app.controller("FeedbackControl", function($scope, $rootScope, $mdToast) {
   $rootScope.navItem="feedback";
-  $rootScope.pageTitle="User Feedback";
+  $rootScope.pageTitle="Feedback / Support";
   $rootScope.tabName="Feedback - HarkerSG";
-  $scope.fb={
-    type: "comment"
-  };
+  $scope.fb={type: "comment"};
   $scope.submit=function() {
     if ($scope.feedback.$valid) {
-      fs.collection("feedback").add({
+      var sendFeedback=functions.httpsCallable("sendFeedback");
+      sendFeedback({
         type: $scope.fb.type,
         text: $scope.fb.text,
-        user: fs.collection("users").doc($scope.uid),
-      })
-        .then(function(doc) {
-          $mdToast.show(
-            $mdToast.simple()
-              .textContent("Successfully submitted feedback.")
-              .hideDelay(4000)
-              .action("OK")
-              .highlightAction(true)
-          );
-        })
-        .catch(function(error) {
-          console.error(error);
-          $mdToast.show(
-            $mdToast.simple()
-              .textContent("An error has occurred.")
-              .hideDelay(4000)
-              .action("OK")
-              .highlightAction(true)
-          );
-        });
-      $scope.fb.type="comment";
-      $scope.fb.text=undefined;
-      $scope.feedback.$setPristine();
-      $scope.feedback.$setUntouched();
-      $scope.fbNo=true;
+        rating: $scope.fb.rating,
+      }).then(function() {
+        $mdToast.show(
+          $mdToast.simple().textContent("Thank you for your feedback.").hideDelay(4000).action("OK").highlightAction(true)
+        );
+        $scope.fb={type: "comment"};
+        $scope.feedback.$setPristine();
+        $scope.feedback.$setUntouched();
+      }).catch(function(error) {
+        console.error(error);
+        $mdToast.show(
+          $mdToast.simple().textContent("An unexpected error has occurred.").hideDelay(4000).action("OK").highlightAction(true)
+        );
+      }).finally(function() {$scope.fbLoading=false;});
+      $scope.fbLoading=true;
+      $mdToast.show(
+        $mdToast.simple().textContent("Sending feedback...").hideDelay(0)
+      );
     }
   };
-});
-app.controller("AdsControl", function($scope, $rootScope) {
-  $rootScope.navItem=null;
-  $rootScope.pageTitle="Submit an Advertisement";
-  $rootScope.tabName="Advertisements - HarkerSG";
 });
